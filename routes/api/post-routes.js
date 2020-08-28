5d645661876f89c5970542aeb4e1c99051e45736
D-Know <n.dino90@gmail.com>
2020-08-27 18:06:17 -0600

    everything done ready to deploy

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const {
   Post,
   User,
-  Vote
+  Vote,
+  Comment
 } = require('../../models');
 const sequelize = require('../../config/connection');
 
@@ -20,7 +21,22 @@ router.get('/', (req, res) => {
       order: [
         ['created_at', 'DESC']
       ],
-      include: [{
+      include: [
+        {
+          model: Comment,
+          attributes: [ 
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at'
+          ],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
         model: User,
         attributes: ['username']
       }]
@@ -44,10 +60,26 @@ router.get('/:id', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-      include: [{
+      include: [
+        {
+          model: Comment,
+          attributes: [ 
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at'
+          ],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
         model: User,
         attributes: ['username']
-      }]
+      }
+    ]
     })
     .then(dbPostData => {
       if (!dbPostData) {
