34d87f12bb9642277d01fe6f78a0f4710e462bc4
Neil Dino <N.dino90@gmail.com>
2020-08-28 13:16:56 -0600

    cannot connect handlebars

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-
-const path = require('path'); //new to add css **notworking**
-
+const path = require('path'); //new to add css **working**
 const express = require('express');
 const routes = require('./controllers'); //new changed file name api endpoints ARE working
 const sequelize = require('./config/connection');
@@ -13,20 +11,33 @@ const hbs = exphbs.create({}); //added for handlebars to view homepage
 
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//When I moved this up here it worked not sure why
+app.use(express.static(path.join(__dirname, 'public')));
+
+//express engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//my previous location
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 //turn on routes
 app.use(routes);
 
 //links to the publice directory (location of CSS file ) **NOT WORKING**
-app.use(express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
