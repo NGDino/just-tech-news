f7d9737d823d194ac639ed1503e5a18b049d5129
Neil Dino <N.dino90@gmail.com>
2020-08-28 20:56:37 -0600

    login logout working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers'); //new changed file name api endpoints ARE working
 const sequelize = require('./config/connection');
 
+require('dotenv').config();
 
 
 const exphbs = require('express-handlebars'); //added for handlebars to view homepage **NOT WORKING**
@@ -14,6 +15,22 @@ const app = express();
 const hbs = exphbs.create({});
 
 const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
